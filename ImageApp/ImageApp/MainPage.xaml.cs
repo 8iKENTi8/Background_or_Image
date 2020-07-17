5d8227dc9e6743ad9a87062d3b32c5bc01eea2fa
Vladmir <roldugin.67@gmail.com>
2020-07-17 14:10:23 +0300

    Добавьте файлы проекта.

---
+++ ImageApp/ImageApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Image1.Source = ImageSource.FromResource("ImageApp.fa.jpg");
+
+            //Image1.Aspect = Aspect.AspectFill;
+            this.BackgroundImageSource = "fa.jpg";
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View());
+        }
+    }
+}
